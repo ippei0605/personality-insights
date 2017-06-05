256dfdc1ad36c96b550771d0ddd2ea99317ce490
Ippei SUZUKI <i.suzuki@jiec.co.jp>
2017-06-05 22:57:21 +0900

    IE対応のためクライアント JavaScript のアロー関数を function に変更

--- public/pi-sample.js
+++ public/pi-sample.js
@@ -1,14 +1,14 @@
-/*
+/**
  * Personality Insights Sample: クライアント JavaScript
  */
 
 'use strict';
 
-$(document).ready(() => {
+$(function () {
     const image = 'https://personality-insights-livedemo.mybluemix.net/images/service-icon.svg';
     const chart = new PersonalitySunburstChart({"selector": "#chartId", "version": "v3"});
 
-    $('#analyzeId').on('click', () => {
+    $('#analyzeId').on('click', function () {
         const resultId = $('#resultId');
         const statusId = $('#statusId');
         let text = $('#textId').val();
@@ -19,12 +19,12 @@ $(document).ready(() => {
             "data": {
                 "text": text
             }
-        }).done((value) => {
+        }).done(function (value) {
             chart.show(value, image);
             statusId.append('<p class="text-info">・・・完了しました。</p>');
-        }).fail((value) => {
+        }).fail(function (value) {
             statusId.append('<p class="text-danger">・・・エラーが発生しました。</p>');
-        }).always((value) => {
+        }).always(function (value) {
             resultId.html('<pre>' + JSON.stringify(value, undefined, 2) + '</pre>');
         });
         return false;
